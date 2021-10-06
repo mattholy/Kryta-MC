27cfab6268cff703c0869c4624208dc1c57bc533
Mattholy <smile.used@hotmail.com>
2021-10-06 21:03:45 +0800

    op

---
+++ app/app/html/frontend-dev/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <Naiveui />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Naiveui from './components/Naiveui.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Naiveui
+  }
+}
+</script>
---
+++ app/app/html/frontend-dev/src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './index.css'
+import naive from 'naive-ui'
+
+createApp(App).use(naive).mount('#app')
