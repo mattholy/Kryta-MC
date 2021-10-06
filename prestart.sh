b037570ef6fb346d7658fe264aaa93bece16f0f8
Mattholy <smile.used@hotmail.com>
2021-10-06 11:21:02 +0800

    op

--- prestart.sh
+++ prestart.sh
@@ -2,7 +2,7 @@
 
 #do some localization, use zh-cn by default
 localize_script_name="${KEYTA_SERVER_LOCATION:-zh-cn}"
-eval . ./utils/init_template/localize_scripts/${localize_script_name}.sh
+eval . ./app/utils/init_template/localize_scripts/${localize_script_name}.sh
 
 apt install sqlite3
 pip install -r requirements.txt
